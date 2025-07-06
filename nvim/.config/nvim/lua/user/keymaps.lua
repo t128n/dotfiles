d24aef0d307e2e4649f04f04bdab03a6d13b5712
Torben Haack <t128n@ipv4.8shield.net>
2025-07-06 12:57:46 +0200

    feat: add keymaps to nvim

---
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,8 @@
+-- Move the current line up or down in Normal mode
+vim.keymap.set('n', '<A-j>', "<cmd>m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set('n', '<A-k>', "<cmd>m .-2<CR>==", { desc = "Move line up" })
+
+-- Move selected lines up or down in Visual mode
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+
